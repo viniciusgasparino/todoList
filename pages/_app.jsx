32a469d036d43cd0780f4d0bc76509f683ce61ff
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-10-09 20:28:43 -0300

    Cirado app e e criado styles.css

---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+import '../style.css'
+
+function App ({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default App
